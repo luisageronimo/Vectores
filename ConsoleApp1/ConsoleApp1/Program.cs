75404ad7777cc55b77c8d44128416e6e11cd0e7e
Luisa Gerónimo <luisageronimo5@gmail.com>
2021-06-07 15:59:24 -0600

    Ejercicio #1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] dato = new char[10];
+
+            //dato[0] = 'a'; 
+
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.WriteLine("i: " + i);
+                Console.Write("Ingrese una letra");
+                dato[i] = char.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
