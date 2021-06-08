7f7ff118888beb9a2519aea43cbed7614dbbf265
Luisa Gerónimo <luisageronimo5@gmail.com>
2021-06-08 15:51:35 -0600

    Complete todo

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,16 +11,38 @@ namespace Ejercicio_1
         static void Main(string[] args)
         {
             char[] dato = new char[10];
+            char buscar;
 
             //dato[0] = 'a'; 
 
             for (int i = 0; i <= 9; i++)
             {
                 Console.WriteLine("i: " + i);
-                Console.Write("Ingrese una letra");
+                Console.Write("Ingrese una letra: ");
                 dato[i] = char.Parse(Console.ReadLine());
             }
+            Console.WriteLine("--------------------------");
 
+            for (int x=0;x<=9 ;x++)
+            {
+                Console.WriteLine(dato[x]);
+            }
+
+            Console.WriteLine("--------------------------");
+
+            Console.Write("Ingrese el caracter a buscar: ");
+            buscar = char.Parse(Console.ReadLine());
+
+            for (int z=0; z<=9; z++)
+            {
+                if(dato[z]==buscar)
+                {
+                    Console.Write("dato " + buscar + " encontrado");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
+
